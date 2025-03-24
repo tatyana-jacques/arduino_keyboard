e4dfd04cd72057822e2429a96e0b1667e54d233b
Tatyana Jacques <taty.aj@gmail.com>
2025-03-24 09:23:26 -0300

    Add readme.

---
+++ README.md
@@ -0,0 +1,21 @@
+<h1>TECLADO MUSICAL</h1>
+<p>Projeto de teclado musical com Arduino na protoboard. Composto por oito botões (teclas) de notas e um botão para trocar o modo de operação. O projeto possui dois modos de operação: </p>
+<ol>
+<li>Tocas notas.</li>
+<li>Tocar melodias.</li>
+</ol>
+<h3>TOCAR NOTAS</h3>
+<p>Cada um dos oitos botões corresponde a uma nota da escala de dó maior. Quando pressionados os botões correspondentes, o buzzer emitirá a frequência da nota definida. </p>
+<h3>TOCAR MELODIAS</h3>
+Quando a função "tocar_melodia" é ativada, o buzzer emitira um trecho musical correspondente ao botão (tecla) pressionado. Serão executados trechos musicais das seguintes músicas:
+<ol> 
+<li>Tecla 01: Ode à Alegria (Beethoven) </li>
+<li>Tecla 02: Jingle Bells </li>
+<li>Tecla 03: Cerejeira em Flor (folclore japonês)</li>
+<li>Tecla 04: Greensleeves (medieval)</li>
+<li>Tecla 05: Serenata Noturna (Mozart)</li>
+<li>Tecla 06: Parabéns pra você</li>
+<li>Tecla 07: Frère Jacques</li>
+<li>Tecla 08: Tico-tico no Fubá (Zequinha de Abreu)</li>
+</ol>
+<img src = "teclado.jpg" width = "500">
--- teclado_musical/tocar_melodia.ino
+++ teclado_musical/tocar_melodia.ino
@@ -273,105 +273,80 @@ void tocarMelodia(int melodia)
     delay(400);
     break;
 
-    /*case 8:
-    Código a ser implementado
-    //1
+    case 8:
     tone(falante,mi3, 100);
     delay(150);
 
-    //2
     tone(falante,re3_sustenido, 100);
     delay(150);
 
-    //3
     tone(falante,mi3, 100);
     delay(150);
 
-    //4
     tone(falante,fa3, 100);
     delay(150);
 
-    //5
     tone(falante,mi3, 100);
     delay(150);
 
-    //6
     tone(falante,la3, 300);
     delay(450);
 
-    //7
     tone(falante,mi3, 100);
     delay(150);
 
-    //8
     tone(falante,re3_sustenido, 100);
     delay(150);
 
-    //9
     tone(falante,mi3, 100);
     delay(150);
 
-    //10
     tone(falante,fa3,100);
     delay(150);
 
-    //11
     tone(falante,mi3,100);
     delay(150);
 
-    //12
     tone(falante,sol3_sustenido, 200);
     delay(450);
 
-    //13
     tone(falante,mi3,100);
     delay(150);
 
-    //14
     tone(falante,re3_sustenido,100);
     delay(150);
 
-    //15
     tone(falante,mi3,100);
     delay(150);
 
-    //16
     tone(falante,fa3,100);
     delay(150);
 
-    //17
     tone(falante,mi3,100);
     delay(150);
 
-    //18
     tone(falante,re4, 100);
     delay(150);
 
-    //19
     tone(falante,si3,100);
     delay(150);
 
-    //20
     tone(falante,sol3_sustenido,100);
     delay(150);
 
-    //21
     tone(falante,mi3, 100);
     delay(150);
 
-    //22
     tone(falante,re3,100);
     delay(150);
 
-    //23
     tone(falante,do3_sustenido,100);
     delay(150);
 
-    //24
     tone(falante,do3,400);
     delay(400);
     break;
 
-    */
+    
   }
 }
